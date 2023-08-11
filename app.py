6d9a81ceb72cb4b989c143e6b72aa2f7d0a3b6c7
20481A5450 <99806742+20481A5450@users.noreply.github.com>
2023-08-11 15:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
+from googleapiclient.discovery import build
 import requests
 import random
-from googleapiclient.discovery import build
 
 app = Flask(__name__)
 
@@ -12,67 +12,73 @@ API_KEY = 'AIzaSyC36BG14zOq3IKtceQGWzr9r1SBWv4ujOE'
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 def get_search_results(keyword, max_results=10):
-    search_response = youtube.search().list(
-        q=keyword,
-        type='video',
-        part='id',
-        maxResults=max_results
-    ).execute()
-
-    video_ids = [item['id']['videoId'] for item in search_response['items']]
-    total_results = search_response.get('pageInfo', {}).get('totalResults', 0)
-    
-    return video_ids, total_results
+    try:
+        search_response = youtube.search().list(
+            q=keyword,
+            type='video',
+            part='id',
+            maxResults=max_results
+        ).execute()
+        video_ids = [item['id']['videoId'] for item in search_response['items']]
+        total_results = search_response.get('pageInfo', {}).get('totalResults', 0)
+        return video_ids, total_results
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return [], 0
 
 def get_video_details(video_id):
-    video_response = youtube.videos().list(
-        part='snippet,statistics',
-        id=video_id
-    ).execute()
+    try:
+        video_response = youtube.videos().list(
+            part='snippet,statistics',
+            id=video_id
+        ).execute()
 
-    return video_response['items'][0]
-
-def get_related_videos(video_id, max_results=5):
-    search_response = youtube.search().list(
-        relatedToVideoId=video_id,
-        type='video',
-        part='id,snippet',
-        maxResults=max_results,
-        key=API_KEY
-    ).execute()
-
-    related_videos = []
-    for item in search_response['items']:
-        video_id = item['id']['videoId']
-        video_title = item['snippet']['title']
-        related_videos.append({'id': video_id, 'title': video_title})
-
-    return related_videos
+        return video_response['items'][0]
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return {}
+def estimate_search_volume(video_data):
+    try:
+        views = int(video_data['statistics'].get('viewCount', 0))
+        likes = int(video_data['statistics'].get('likeCount', 0))
+        comments = int(video_data['statistics'].get('commentCount', 0))
+        
+        # Relative weights for engagement metrics (you can adjust these weights)
+        view_weight = 0.6
+        like_weight = 0.3
+        comment_weight = 0.1
+        
+        # Calculate estimated search volume based on engagement metrics
+        search_volume = views * view_weight + likes * like_weight + comments * comment_weight
+        
+        return int(search_volume)
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return 0
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     search_results = []
     video_details = {}
-    related_videos = []
     search_volume = 0
     graph_data = []
 
     if request.method == 'POST':
         keyword = request.form.get('keyword')
         if keyword:
-            search_results, search_volume = get_search_results(keyword)
+            search_results, total_results = get_search_results(keyword)
+            search_volume = total_results
             
             if search_results:
                 video_id = search_results[0]
                 video_details = get_video_details(video_id)
-                related_videos = get_related_videos(video_id)
+                search_volume = estimate_search_volume(video_details)
                 
-                # Generate random search counts for the past 12 months (placeholder data)
+                # Generate placeholder search volume data for the past 12 months
                 graph_data = [random.randint(1000, 10000) for _ in range(12)]
 
     return render_template('index.html', search_results=search_results, search_volume=search_volume,
-                       graph_data=graph_data, video_details=video_details, related_videos=related_videos)
+                           graph_data=graph_data, video_details=video_details)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
+    app.run(debug=False, host='0.0.0.0')
